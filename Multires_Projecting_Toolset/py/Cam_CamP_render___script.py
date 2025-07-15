63a9e22898307933aee188a4c2d23820d8e88ade
Packsod <142358787+Packsod@users.noreply.github.com>
2025-07-15 18:37:54 +0800

    Update base_path with camera name

--- Multires_Projecting_Toolset/py/Cam_CamP_render___script.py
+++ Multires_Projecting_Toolset/py/Cam_CamP_render___script.py
@@ -70,9 +70,7 @@ class RenderSelectedCamPOperator(bpy.types.Operator):
         original_base_path = output_path_node.base_path
 
         # append camera name to base_path
-        if not output_path_node.base_path.endswith(('/', '\\')):
-            output_path_node.base_path += '/'
-        output_path_node.base_path += camera_name
+        output_path_node.base_path = os.path.join(output_path_node.base_path, camera_name)
 
         """
         Note that // means a network path in Windows,
